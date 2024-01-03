25a7c1d2cda03cf669ba3926300c2188be62d319
DevBasem <engr.basemahmed@gmail.com>
2024-01-03 11:16:42 -0800

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-if ARGV.length == 1
-  input = ARGV[0]
-  regex = /hb(t{2,5})n/
-
-  matches = input.scan(regex)
-
-  if matches.any?
-    puts matches.join("\n")
-  else
-    puts "No match"
-  end
-else
-  puts "Usage: #{$PROGRAM_NAME} <input>"
-  exit 1
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
