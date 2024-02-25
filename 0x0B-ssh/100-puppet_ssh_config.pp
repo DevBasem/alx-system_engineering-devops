fbe13191c8c80800ff6a04169821c002bb49e676
DevBasem <engr.basemahmed@gmail.com>
2024-02-25 14:31:44 -0800

    task 1 adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,4 @@
-# 100-puppet_ssh_config.pp
-
-class { 'stdlib': }
-
 # Ensure SSH client configuration
-include stdlib
-
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path => '/etc/ssh/sshd_config',
@@ -15,11 +9,4 @@ file_line { 'Declare identity file':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
-}
-
-# Notify SSH service to reload configuration
-service { 'ssh':
-  ensure    => running,
-  enable    => true,
-  subscribe => File_line['Turn off passwd auth', 'Declare identity file'],
 }
