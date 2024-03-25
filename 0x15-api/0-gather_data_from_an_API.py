409b31f86f90b2d8d5ea1777f0988684a833d712
DevBasem <engr.basemahmed@gmail.com>
2024-03-25 12:08:22 -0700

    task 0 fx

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-their TODO list progress"""
+"""Retrieve information about an employee's TODO list progress
+using the JSONPlaceholder REST API."""
 
 import requests
 import sys
 
-if __name__ == "__main__":
+def get_todo_progress(employee_id):
+    """
+    Retrieve and display information about an employee's TODO list progress.
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
+    Args:
+        employee_id (int): The ID of the employee.
 
-    name = user.json().get('name')
+    Returns:
+        None
+    """
+    user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                                 .format(employee_id))
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    totalTasks = 0
+    todos_response = requests.get('https://jsonplaceholder.typicode.com/todos')
+    total_tasks = 0
     completed = 0
 
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
+    for task in todos_response.json():
+        if task.get('userId') == int(employee_id):
+            total_tasks += 1
             if task.get('completed'):
                 completed += 1
 
     print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
+          .format(employee_name, completed, total_tasks))
 
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
+    for task in todos_response.json():
+        if task.get('userId') == int(employee_id) and task.get('completed'):
+            print('\t{}'.format(task.get("title")))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    get_todo_progress(employee_id)
