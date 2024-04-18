6915eafe846ec8fc0408eb167a921055beec64c4
DevBasem <engr.basemahmed@gmail.com>
2024-04-18 13:58:49 -0700

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,3 @@
-ncreases the amount of traffic an Nginx server can handle.
-
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,3 @@
-ble the user holberton to login.
-
 # Increase hard file limit for Holbe1-user_limit.pprton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
